b0c44563c2cb03194ed8b658548fb5d0ec82f1e1
Sequoia McDowell <sequoia.mcdowell@gmail.com>
2015-12-07 16:27:39 -0500

    adding validation

--- common/models/coffee-shop.js
+++ common/models/coffee-shop.js
@@ -1,3 +1,12 @@
 module.exports = function(CoffeeShop) {
+  CoffeeShop.validatesPresenceOf('name');
 
+  CoffeeShop.validatesLengthOf('name',{
+      min: 5,
+      max: 100,
+      message: {
+          min: 'Name is too short',
+          max: 'Name is too long'
+      }
+  });
 };
--- common/models/review.js
+++ common/models/review.js
@@ -1,3 +1,13 @@
 module.exports = function(Review) {
 
+    Review.validate('rating', validateRating, {
+      message: 'Please enter a rating from 1 to 5'
+    });
+    
+    function validateRating(errorCallback) {
+        var rating = Number(this.rating);
+        if (!rating || rating < 1 || rating > 5) {
+            errorCallback();
+        }
+    }
 };
